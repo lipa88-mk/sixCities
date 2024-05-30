1f8dadc0f9d58202e92cedae74ca9b7f54c5785a
lipa88-mk <po4taimenimenia@gmail.com>
2024-05-30 11:48:35 +0200

    add Routes in App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainScreen } from '../../pages/main-screen/main-screen';
+import { FavoritesScreen } from '../../pages/favorites-screen/favorites-screen';
+import { LoginScreen } from '../../pages/login-screen/login-screen';
+import { PropertyScreen } from '../../pages/property-screen/property-screen';
 
 type AppProps = {
   offersCount: number;
-}
+};
 
-function App({offersCount}: AppProps): JSX.Element {
-  return <MainScreen offersCount={offersCount} />;
-}
+const App = ({ offersCount }: AppProps): JSX.Element => (
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<MainScreen offersCount={offersCount} />} />
+      <Route path="/login" element={<LoginScreen />}>
+        <Route path="/favorites" element={<FavoritesScreen />} />
+      </Route>
+      <Route path="/offer/:id" element={<PropertyScreen />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
