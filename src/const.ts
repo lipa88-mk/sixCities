d1b36506ca98201cc07127fcec5a3b9525fa1aa5
lipa88-mk <po4taimenimenia@gmail.com>
2024-09-30 13:11:39 +0200

    fix map markers

--- src/const.ts
+++ src/const.ts
@@ -14,9 +14,10 @@ export enum AuthorizationStatus {
 export const cities = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'] as const;
 
 export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+  './img/pin.svg';
+
 export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+  './img/pin-active.svg';
 
 export const Settings = {
   STAR_RATING_MAX: 5,
