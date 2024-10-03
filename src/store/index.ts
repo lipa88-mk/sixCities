6ec89fefe5298dab30f4622a1a8c1eef140f8ed0
lipa88-mk <po4taimenimenia@gmail.com>
2024-10-03 13:22:30 +0200

    use axios in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import { reducer } from './reducer';
 import { setOffers, setReviews } from './action';
+import { createAPI } from '../services/api';
+
 import { offers } from '../mocks/offers';
 import { reviews } from '../mocks/reviews';
 
-const store = configureStore({
-  reducer
+export const api = createAPI();
+
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
 
 // ToDo: replace with server data
 store.dispatch(setOffers(offers));
 store.dispatch(setReviews(reviews));
 
-export default store;
