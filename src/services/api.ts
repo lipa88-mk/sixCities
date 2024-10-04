2bf82aa8e9b004245811dd2b1ef14aa04e0fe1ea
lipa88-mk <po4taimenimenia@gmail.com>
2024-10-04 15:36:29 +0200

    add token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
 const BACKEND_URL = 'https://10.react.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -8,5 +9,14 @@ export const createAPI = (): AxiosInstance => {
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = getToken();
+    if (token && config.headers) {
+      config.headers['X-Token'] = token;
+    }
+    return config;
+  });
+
   return api;
 };
