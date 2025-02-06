ded54110e444ed18dc45f54cf0e1041482ec92dc
Maria Korshunova <65017545+lipa88-mk@users.noreply.github.com>
2025-02-06 12:31:38 +0100

    main empty screen refactoring

--- src/components/cards-list/cards-list.tsx
+++ src/components/cards-list/cards-list.tsx
@@ -1,29 +1,23 @@
 import { FC, useState } from 'react';
 import Card from '../card/card';
-import { useAppDispatch, useAppSelector } from '../../hooks';
+import { useAppDispatch } from '../../hooks';
 import { Map } from '../../components/map/Map';
 import { SortingList } from '../../components/sorting-list/sorting-list';
-import { SortName } from '../../types/types';
+import { Offer, SortName, CityPlacement } from '../../types/types';
 import { setSorting } from '../../store/action';
-import { SortingFunctions } from '../../const';
-import { Spinner } from '../spinner/spinner';
 
-const CardsList: FC = () => {
+type CardsListProps = {
+  activeSorting: SortName;
+  offers: Offer[];
+  activeCity: CityPlacement;
+};
+
+const CardsList: FC<CardsListProps> = ({
+  activeSorting,
+  offers,
+  activeCity,
+}) => {
   const dispatch = useAppDispatch();
-  const activeSorting = useAppSelector((state) => state.sorting);
-
-  const activeCity = useAppSelector((state) => state.city);
-  const offers = useAppSelector(
-    (state) => state.offers.filter(
-      (offer) => offer.city.name === state.city.name
-    ).sort(SortingFunctions[state.sorting])
-  );
-
-  const isOffersLoading = useAppSelector((state) => state.isOffersLoading);
-
-  // ToDo: add MainEmptyScreen
-  // const isEmpty = !isOffersLoading && offers.length === 0;
-
   const [activeOffer, setActiveOffer] = useState<number | null>(null);
 
   const handleCardMouseMove = (id: number) => {
@@ -38,8 +32,6 @@ const CardsList: FC = () => {
     dispatch(setSorting(name));
   };
 
-  if (isOffersLoading) {return <Spinner />;}
-
   return (
     <>
       <section className="cities__places places">
@@ -48,7 +40,10 @@ const CardsList: FC = () => {
           {offers.length} places to stay in {activeCity.name}
         </b>
 
-        <SortingList currentSorting={activeSorting} onChange={handleSortingChange}/>
+        <SortingList
+          currentSorting={activeSorting}
+          onChange={handleSortingChange}
+        />
 
         <div className="cities__places-list places__list tabs__content">
           {offers.map((offer) => (
@@ -63,7 +58,11 @@ const CardsList: FC = () => {
       </section>
 
       <div className="cities__right-section">
-        <Map locations={offers.map(({ id, location }) => ({ id, ...location }))} city={activeCity} activeOffer={activeOffer} />
+        <Map
+          locations={offers.map(({ id, location }) => ({ id, ...location }))}
+          city={activeCity}
+          activeOffer={activeOffer}
+        />
       </div>
     </>
   );
--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -4,12 +4,21 @@ import Logo from '../../components/logo/logo';
 import CardsList from '../../components/cards-list/cards-list';
 import { CitiesList } from '../../components/cities-list/cities-list';
 import { useAppSelector } from '../../hooks';
+import { SortingFunctions } from '../../const';
+import { Spinner } from '../../components/spinner/spinner';
+import { MainEmptyScreen } from './main-screen-empty';
 
 const MainScreen: FC = () => {
+  const activeSorting = useAppSelector((state) => state.sorting);
+
+  const activeCity = useAppSelector((state) => state.city);
   const offers = useAppSelector((state) =>
-    state.offers.filter((offer) => offer.city.name === state.city.name)
+    state.offers
+      .filter((offer) => offer.city.name === state.city.name)
+      .sort(SortingFunctions[state.sorting])
   );
-  const isEmpty = offers.length === 0;
+  const isOffersLoading = useAppSelector((state) => state.isOffersLoading);
+  const isEmpty = !isOffersLoading && !offers;
 
   return (
     <div className="page page--gray page--main">
@@ -26,7 +35,11 @@ const MainScreen: FC = () => {
         </div>
       </header>
 
-      <main className={`page__main page__main--index ${isEmpty ? 'page__main--index-empty' : '' }`}>
+      <main
+        className={`page__main page__main--index ${
+          isEmpty ? 'page__main--index-empty' : ''
+        }`}
+      >
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -35,9 +48,21 @@ const MainScreen: FC = () => {
         </div>
         <div className="cities">
           <div
-            className={`cities__places-container container ${isEmpty ? 'cities__places-container--empty' : '' }`}
+            className={`cities__places-container container ${
+              isEmpty ? 'cities__places-container--empty' : ''
+            }`}
           >
-            <CardsList />
+            {isOffersLoading && <Spinner />}
+
+            {!isOffersLoading && isEmpty && <MainEmptyScreen />}
+
+            {!isOffersLoading && !isEmpty && (
+              <CardsList
+                activeSorting={activeSorting}
+                offers={offers}
+                activeCity={activeCity}
+              />
+            )}
           </div>
         </div>
       </main>
