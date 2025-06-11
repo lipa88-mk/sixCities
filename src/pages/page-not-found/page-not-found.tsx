b6068e2d8f3b07081cf430e595da5c3688af323a
Maria Korshunova <65017545+lipa88-mk@users.noreply.github.com>
2025-06-11 16:05:43 +0200

    update tests

--- src/pages/page-not-found/page-not-found.tsx
+++ src/pages/page-not-found/page-not-found.tsx
@@ -1,11 +1,10 @@
-import { Link } from 'react-router-dom';
 import './style.css';
 import { FC } from 'react';
 
 const PageNotFound: FC = () => (
   <main className='container-404'>
     <h1>404 Not Found</h1>
-    <Link to={'/'} className='container-404__link'>Go back Home</Link>
+    <a href={'/'} className='container-404__link'>Go back Home</a>
   </main>
 );
 
