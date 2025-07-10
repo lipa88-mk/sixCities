8a05149ae4a32de6a95139b1c390fdb5ef0422a7
Maria Korshunova <65017545+lipa88-mk@users.noreply.github.com>
2025-07-10 16:34:04 +0200

    prepare deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
-    base: '',
+    base: '/sixCities/',
     plugins: [
       tanstackRouter({
         target: 'react',
